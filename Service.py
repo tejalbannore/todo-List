88bea73d62ee383e22458700abee68b1c802c156
Tejal Bannore <tejalbannore@gmail.com>
2021-01-12 15:46:01 +0530

    first commit

---
+++ Service.py
@@ -0,0 +1,7 @@
+# Service.py
+class ToDoService:
+    def __init__(self):
+        self.model = ToDoModel()
+
+    def create(self, params):
+        self.model.create(params["text"], params["Description"])
